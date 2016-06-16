3bd536c3ff0b110c93f7601e6546fe18cc2737f0
perlancar (@pc-office) <perlancar@gmail.com>
2016-06-16 14:02:41 +0700

    Allow customizing allowed methods & return value of dumb object

--- lib/Object/Dumb.pm
+++ lib/Object/Dumb.pm
@@ -3,11 +3,34 @@ package Object::Dumb;
 # DATE
 # VERSION
 
-sub new { my $o = ""; bless \$o, shift }
-sub AUTOLOAD { 0 }
+#use strict;
+#use warnings;
+
+sub new {
+    my $class = shift;
+    my $o = {@_};
+
+    $o->{returns} = 0 if !exists($o->{returns});
+    bless $o, $class;
+}
+
+sub AUTOLOAD {
+    my $meth = $AUTOLOAD; $meth =~ s/.+:://;
+    my $self = shift;
+    if ($self->{methods}) {
+        my $known = 0;
+        if (ref($self->{methods}) eq 'ARRAY') {
+            $known = 1 if grep { $_ eq $meth } @{ $self->{methods} };
+        } elsif (ref($self->{methods}) eq 'Regexp') {
+            $known = 1 if $meth =~ $self->{methods};
+        }
+        die "Unknown method '$meth'" unless $known;
+    }
+    return $self->{returns};
+}
 
 1;
-# ABSTRACT: A dumb object that does nothing when you call any of its methods
+# ABSTRACT: A dumb object that responds to any method and just returns 0
 
 =for Pod::Coverage ^(.+)$
 
@@ -19,8 +42,33 @@ sub AUTOLOAD { 0 }
  $obj->foo;          # -> 0
  $obj->bar(1, 2, 3); # -> 0
 
+You can limit what methods will be available:
+
+ my $obj = Object::Dumb->new(methods => [qw/foo bar/]);
+ $obj->foo; # ok
+ $obj->bar; # ok
+ $obj->baz; # dies
+
+or:
+
+ my $obj = Object::Dumb->new(methods => qr/^(foo.*|bar.+)$/);
+ $obj->foo;  # ok
+ $obj->barb; # ok
+ $obj->baz;  # dies
+
+And you can also customize what value the methods will return:
+
+ my $obj = Object::Dumb->new(returns => 1);
+ print $obj->foo; # 1
+
 
 =head1 DESCRIPTION
+
+This module lets you create a "dumb" object that responds to any method and just
+returns 0.
+
+You can customize by limiting what methods the object will respond to, and what
+value the methods will return.
 
 
 =head1 SEE ALSO
